fa9caf5816c5af1c2ebb2da61c7f7e603cc91e75
Vador92 <119339398+Vador92@users.noreply.github.com>
2025-09-25 09:19:50 -0400

    added another movement bind. potentionally might add a second player, if I do not include mouse aim

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ screen = pg.display.set_mode((800, 800))
 clock = pg.time.Clock()
 running = True
 
+screen_rect = screen.get_rect()
+
 class Player:
     def __init__(self, position, size):
         self.position : pg.Vector2 = position
@@ -12,10 +14,13 @@ class Player:
         self.speed = 200
     
     def draw(self):
-        pg.draw.circle(screen, "green", self.position, self.size).clamp_ip(screen.get_rect())
+        pg.draw.circle(screen, "green", self.position, self.size)
+    
+    def clamp_player(self):
+        self.position.x = max(self.size, min(self.position.x, screen.get_width() - self.size))
+        self.position.y = max(self.size, min(self.position.y, screen.get_height() - self.size))
 
-
-player = Player(pg.Vector2(300,300), 20)
+player = Player(pg.Vector2(50,50), 20)
 
 while running:
     dt = clock.tick(60) / 1000
@@ -24,15 +29,16 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    if pg.key.get_pressed()[pg.K_LEFT]:
+    if pg.key.get_pressed()[pg.K_LEFT] or pg.key.get_pressed()[pg.K_a]:
         player.position.x -= player.speed * dt
-    if pg.key.get_pressed()[pg.K_RIGHT]:
+    if pg.key.get_pressed()[pg.K_RIGHT] or pg.key.get_pressed()[pg.K_d]:
         player.position.x += player.speed * dt 
-    if pg.key.get_pressed()[pg.K_DOWN]:
+    if pg.key.get_pressed()[pg.K_DOWN] or pg.key.get_pressed()[pg.K_s]:
         player.position.y += player.speed * dt    
-    if pg.key.get_pressed()[pg.K_UP]:
+    if pg.key.get_pressed()[pg.K_UP] or pg.key.get_pressed()[pg.K_w]:
         player.position.y -= player.speed * dt
 
+    player.clamp_player()
     # game rendering done here
     screen.fill("black")
     player.draw()
