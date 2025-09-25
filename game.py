785bf56ffe02df0431d95e5e03b0d1b23314f33b
Vador92 <119339398+Vador92@users.noreply.github.com>
2025-09-25 08:53:01 -0400

    added some basic movement

--- game.py
+++ game.py
@@ -1,26 +1,41 @@
 import pygame as pg
 
 pg.init()
-screen = pg.display.set_mode((600, 400))
+screen = pg.display.set_mode((800, 800))
 clock = pg.time.Clock()
 running = True
 
-active_bullets = []
+class Player:
+    def __init__(self, position, size):
+        self.position : pg.Vector2 = position
+        self.size = size
+        self.speed = 200
+    
+    def draw(self):
+        pg.draw.circle(screen, "green", self.position, self.size).clamp_ip(screen.get_rect())
+
+
+player = Player(pg.Vector2(300,300), 20)
 
 while running:
+    dt = clock.tick(60) / 1000
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
-    screen.fill("black")
+    if pg.key.get_pressed()[pg.K_LEFT]:
+        player.position.x -= player.speed * dt
+    if pg.key.get_pressed()[pg.K_RIGHT]:
+        player.position.x += player.speed * dt 
+    if pg.key.get_pressed()[pg.K_DOWN]:
+        player.position.y += player.speed * dt    
+    if pg.key.get_pressed()[pg.K_UP]:
+        player.position.y -= player.speed * dt
 
     # game rendering done here
-    active_bullets.append()
-
-
+    screen.fill("black")
+    player.draw()
     pg.display.flip()
-    
-
-    clock.tick(60)
 
 pg.quit()
